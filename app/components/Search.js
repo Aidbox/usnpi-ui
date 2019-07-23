76c0b315a95a972ce1212d51e9ce7470bc592fb4
TheZorkij <zorkijofficial@gmail.com>
2019-07-23 15:06:53 +0300

    non-accordion collapsible

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,19 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Input, List, Tabs, Descriptions, Table } from 'antd';
+import { Input, List, Tabs, Descriptions, Table, Collapse } from 'antd';
 import { site_url } from '../config';
 import useStoreon from 'storeon/react';
 
 const { Search } = Input;
 const { TabPane } = Tabs;
-
+const { Panel } = Collapse;
 
 export const SearchResource = () => {
-  //const [result, setResult] = useState({
-  //  name: [],
-  //  info: [],
-  //});
   const [result, setResult] = useState([]);
-
   const getData = input => {
       let query = `${site_url}/Practitioner`;
       if (input != '')
@@ -33,28 +28,33 @@ export const SearchResource = () => {
       <Tabs defaultActiveKey='1' >
         <TabPane tab="Practitioner" key="1">
           <Search placeholder="Search..." style={{marginBottom: '6px'}} enterButton="Search" size="large" onSearch={value => getData(value)} />
-          <List size="large" pagination={{pageSize: 10}} dataSource={result} renderItem={ (item) => (
-            <List.Item style={{padding: '8px 0px 6px 0px'}}>
-              <List.Item.Meta
-                title={
-                  <div className="container">
-                    <div className="name">
-                      {item.resource.name[0].given.join(' ') + ' ' + item.resource.name[0].family}
-                    </div>
-                    <div className="qual">{item.resource.qualification[0].code.text}</div>
-                  </div>
-                }
-                description={
-                  <div className="container" id="desc">
-                    <div className="address">
-                      {item.resource.address[0].line + ', ' + item.resource.address[0].city + ', ' + item.resource.address[0].state}
-                    </div>
-                    <div className="id">
-                      {item.resource.identifier[0].value}
-                    </div>
-                  </div>
-                } />
-            </List.Item>
+          <List size="large" pagination={{pageSize: 10}} dataSource={result} renderItem={ (item, i) => (
+            <Collapse bordered={false}>
+              <Panel showArrow={false} key={i} header={
+                <List.Item >
+                  <List.Item.Meta
+                    title={
+                      <div className="container">
+                        <div className="name">
+                          {item.resource.name[0].given.join(' ') + ' ' + item.resource.name[0].family}
+                        </div>
+                        <div className="qual">{item.resource.qualification[0].code.text}</div>
+                      </div>
+                    }
+                    description={
+                      <div className="container" id="desc">
+                        <div className="info">
+                          {item.resource.address[0].line + ', ' + item.resource.address[0].city + ', ' + item.resource.address[0].state}&nbsp;&nbsp;{item.resource.telecom[0].value}
+                        </div>
+                        <div className="info">
+                          {item.resource.identifier[0].value}
+                        </div>
+                      </div>
+                    } />
+                </List.Item>}>
+                {i}
+              </Panel>
+            </Collapse>
           )}/>
         </TabPane>
         <TabPane tab="Organization" key="2">
