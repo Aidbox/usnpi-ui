15c93632d80e2763d948c772fad9c3cd9ec16519
TheZorkij <zorkijofficial@gmail.com>
2019-07-26 11:47:18 +0300

    fix attempt 3

--- app/index.js
+++ app/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Router } from 'react-router-dom'
 import history from './history'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 import App from './App'
