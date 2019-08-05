8372f3a14de124486b20cf79a60c3448e40b5923
TheZorkij <zorkijofficial@gmail.com>
2019-08-05 17:33:19 +0300

    footer cosmetic

--- app/App.js
+++ app/App.js
@@ -38,22 +38,30 @@ const App = () => (
           minHeight: '120px',
           paddingBottom: '10px',
         }}>
-            <div style={{marginRight: '3rem'}}>
-              <a className="credits" href="https://www.health-samurai.io/aidbox" >
+          <div style={{marginRight: '3rem'}}>
+            <a className="credits" href="https://www.health-samurai.io/aidbox" >
               <div style={{
                 fontFamily: "'Gotham Pro', Arial, Sans-Serif",
                 fontWeight: "800",
                 fontSize: "18px",
                 lineHeight: "30px",
-                color: "#404040",
+                color: "#515151",
               }}>Powered by </div>
-              <img src="https://uploads-ssl.webflow.com/57441aa5da71fdf07a0a2e19/5a627061e27c0000017f097c_new-logo-aidbox.svg" />
-              </a>
-            </div>
-          <a className="credits" href="https://www.health-samurai.io/">
-          <div>
-            <img style={{paddingTop: '2rem', paddingBottom: '1rem'}} src="https://uploads-ssl.webflow.com/57441aa5da71fdf07a0a2e19/5a5530dbcf0636000108521f_new-hslogo.svg" />
+              <img style={{
+                opacity: '0.9',
+                filter: 'alpha(opacity=90)',
+              }} src="https://uploads-ssl.webflow.com/57441aa5da71fdf07a0a2e19/5a627061e27c0000017f097c_new-logo-aidbox.svg" />
+            </a>
           </div>
+          <a className="credits" href="https://www.health-samurai.io/">
+            <div>
+              <img style={{
+                opacity: '0.9',
+                filter: 'alpha(opacity=90)',
+                paddingTop: '2rem',
+                paddingBottom: '1rem'
+              }} src="https://uploads-ssl.webflow.com/57441aa5da71fdf07a0a2e19/5a5530dbcf0636000108521f_new-hslogo.svg" />
+            </div>
           </a>
         </div>
       </div>
